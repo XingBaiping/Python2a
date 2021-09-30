99eec8ac7626fc6d6a72dcbdbb2b02b6f94364d4
Xing <BXING1@volvocars.com>
2021-09-30 16:57:25 +0800

    assignment1 finishedgit add assignment1.py!

---
+++ week1/assignment1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print(np.arange(10))
+
+list_of_numbers = [[1, 2, 3, 4, 5, 6], [11, 12, 13, 14, 15, 16], [21, 22, 23, 24, 25, 26]]
+print("Array before transpose is :")
+print(list_of_numbers)
+print("Array after transpose is :")
+print(np.transpose(list_of_numbers))
+print("Assignment1 finished!!!!!!")
